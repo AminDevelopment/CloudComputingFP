ae09fcad4203a7da97e4b4a41ed89f99884aebb0
AminDevelopment <nabeel.amin4@gmail.com>
2023-04-24 16:50:03 -0400

    adding in additional files

---
+++ Archive/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeNavBar from './components/HomeNavBar';
+
+function App() {
+  return (
+    <>
+      <HomeNavBar />
+    </>
+  );
+}
+
+export default App;
